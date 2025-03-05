3495ee5a6f6fa7478ef3026241090dcc5583781e
Cath-Bo <c.b.p.i.v.a@gmail.com>
2025-03-05 22:31:35 +0100

    change components properties, add functionality to checkboxes

--- project2/app/src/main/java/com/kitten/checkboxandmore/MainActivity.kt
+++ project2/app/src/main/java/com/kitten/checkboxandmore/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kitten.checkboxandmore
 
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,15 +11,35 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     lateinit var result : TextView
+    lateinit var male : CheckBox
+    lateinit var femail : CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        result = findViewById(R.id.TextViewResult)
+        male = findViewById(R.id.CheckBoxMale)
+        femail = findViewById(R.id.checkBoxFemale)
+
+        male.setOnClickListener{
+            if(male.isChecked) {
+                result.setText("Your gender is male")
+                femail.isChecked = false
+            }
+            else{
+                result.setText("What is your gender?")
+            }
         }
+        femail.setOnClickListener{
+            if(femail.isChecked){
+                result.text = "Your gender is female"
+                male.isChecked = false
+            }
+            else{
+                result.text = "What is your gender?"
+            }
+        }
+
     }
 }
