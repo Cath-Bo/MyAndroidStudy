7879c0443013fc2db739713413a94242adf8abb7
Cath-Bo <c.b.p.i.v.a@gmail.com>
2025-03-06 17:33:53 +0100

    add functionality: change of colors for layout

--- project2/app/src/main/java/com/kitten/checkboxandmore/MainActivity.kt
+++ project2/app/src/main/java/com/kitten/checkboxandmore/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.kitten.checkboxandmore
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CheckBox
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -14,6 +19,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var male : CheckBox
     lateinit var femail : CheckBox
 
+    lateinit var green : RadioButton
+    lateinit var red : RadioButton
+    lateinit var yellow : RadioButton
+    lateinit var changeColor : Button
+    lateinit var layout : ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +32,13 @@ class MainActivity : AppCompatActivity() {
         result = findViewById(R.id.TextViewResult)
         male = findViewById(R.id.CheckBoxMale)
         femail = findViewById(R.id.checkBoxFemale)
+
+        green = findViewById(R.id.green)
+        yellow = findViewById(R.id.yellow)
+        red = findViewById(R.id.red)
+
+        changeColor = findViewById(R.id.buttonChangeColor)
+        layout = findViewById(R.id.constraintLayout)
 
         male.setOnClickListener{
             if(male.isChecked) {
@@ -39,6 +56,17 @@ class MainActivity : AppCompatActivity() {
             }
             else{
                 result.text = "What is your gender?"
+            }
+        }
+        changeColor.setOnClickListener{
+            if(red.isChecked){
+                layout.setBackgroundColor(Color.RED)
+            }
+            else if(green.isChecked){
+                layout.setBackgroundColor(Color.GREEN)
+            }
+            else if(yellow.isChecked){
+                layout.setBackgroundColor(Color.YELLOW)
             }
         }
 
